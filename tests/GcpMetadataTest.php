c953496dec862d72e2688a0bd9bc817dd757a3a1
Jérôme Gamez <jerome@gamez.name>
2026-04-27 14:45:17 +0200

    Update PHPUnit

--- tests/GcpMetadataTest.php
+++ tests/GcpMetadataTest.php
@@ -9,24 +9,22 @@ use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Kreait\GcpMetadata\Error;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class GcpMetadataTest extends TestCase
 {
-    private $client;
-    private $metadata;
+    private ClientInterface $client;
+    private GcpMetadata $metadata;
 
     protected function setUp(): void
     {
-        $this->client = $this->createMock(ClientInterface::class);
+        $this->client = $this->createStub(ClientInterface::class);
 
         $this->metadata = new GcpMetadata($this->client);
     }
 
-    /**
-     * @test
-     */
-    public function it_uses_a_default_client(): void
+    public function testItUsesADefaultClient(): void
     {
         $metadata = new GcpMetadata();
 
@@ -35,20 +33,14 @@ class GcpMetadataTest extends TestCase
         $metadata->instance();
     }
 
-    /**
-     * @test
-     */
-    public function it_is_available(): void
+    public function testItIsAvailable(): void
     {
         $this->client->method('request')->willReturn($this->createResponse());
 
         $this->assertTrue($this->metadata->isAvailable());
     }
 
-    /**
-     * @test
-     */
-    public function it_is_not_available(): void
+    public function testItIsNotAvailable(): void
     {
         $this->client->method('request')
             ->willThrowException(new ConnectException('Connection refused', new Request('GET', GcpMetadata::baseUrl)));
@@ -56,20 +48,14 @@ class GcpMetadataTest extends TestCase
         $this->assertFalse($this->metadata->isAvailable());
     }
 
-    /**
-     * @test
-     */
-    public function it_is_requires_certain_http_response_headers(): void
+    public function testItRequiresCertainHttpResponseHeaders(): void
     {
         $this->client->method('request')->willReturn(new Response(200));
 
         $this->assertFalse($this->metadata->isAvailable());
     }
 
-    /**
-     * @test
-     */
-    public function it_requires_a_successful_http_response(): void
+    public function testItRequiresASuccessfulHttpResponse(): void
     {
         $this->client->method('request')->willReturn($this->createResponse(500, 'details'));
 
@@ -77,11 +63,8 @@ class GcpMetadataTest extends TestCase
         $this->metadata->instance();
     }
 
-    /**
-     * @test
-     * @dataProvider responseStrings
-     */
-    public function it_parses_http_responses_containing($expectedResult, $responseString): void
+    #[DataProvider('responseStrings')]
+    public function testItParsesHttpResponsesContaining($expectedResult, $responseString): void
     {
         $this->client->method('request')->willReturn($this->createResponse(200, $responseString));
 
@@ -89,21 +72,7 @@ class GcpMetadataTest extends TestCase
         $this->assertSame($expectedResult, $this->metadata->project('foo'));
     }
 
-    /**
-     * @test
-     */
-    public function it_caches_its_results(): void
-    {
-        $this->client
-            ->expects($this->once())
-            ->method('request')
-            ->willReturn($this->createResponse());
-
-        $this->metadata->isAvailable();
-        $this->metadata->isAvailable();
-    }
-
-    public function responseStrings(): array
+    public static function responseStrings(): array
     {
         return [
             'an empty body' => ['', null],
